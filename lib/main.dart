3606b28a057a9c66c8d029e65f81ed29d0c4f6e5
Dirk Volschenk <47568329+RubberBullets@users.noreply.github.com>
2020-03-03 20:59:31 +0200

    'refactored

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,8 @@ class MyBiGFiveQuizApp extends StatefulWidget {
 class MyBiGFiveQuizAppState extends State<MyBiGFiveQuizApp> {
   int questionIndex = 0;
 
+  //list of a question mapped with text,score & QN//
+
   var questions = [
     {
       'question number': 1,
@@ -271,6 +273,8 @@ class MyBiGFiveQuizAppState extends State<MyBiGFiveQuizApp> {
     },
   ];
 
+  //Button Functions that set value on press to scoring function//
+
   void nextQuestionD() {
     print('first question score= ' + questions[0]['score'].toString());
 
@@ -315,6 +319,8 @@ class MyBiGFiveQuizAppState extends State<MyBiGFiveQuizApp> {
     });
   }
 
+//Scoring functions for big 5 segments//
+
   extroversion() {
     return (20 +
         questions[0]['score'] -
@@ -328,6 +334,59 @@ class MyBiGFiveQuizAppState extends State<MyBiGFiveQuizApp> {
         questions[40]['score'] -
         questions[45]['score']);
   }
+  agreeableness() {
+    return (14 -
+        questions[1]['score'] +
+        questions[6]['score'] -
+        questions[11]['score'] +
+        questions[16]['score'] -
+        questions[21]['score'] +
+        questions[26]['score'] -
+        questions[31]['score'] +
+        questions[36]['score'] -
+        questions[41]['score'] +
+        questions[46]['score']);
+  }
+  conscientiousness() {
+    return (14 +
+        questions[2]['score'] -
+        questions[7]['score'] +
+        questions[12]['score'] -
+        questions[17]['score'] +
+        questions[22]['score'] -
+        questions[27]['score'] +
+        questions[32]['score'] -
+        questions[37]['score'] +
+        questions[42]['score'] -
+        questions[47]['score']);
+  }
+  neuroticism() {
+    return (38 -
+        questions[3]['score'] +
+        questions[8]['score'] -
+        questions[13]['score'] +
+        questions[18]['score'] -
+        questions[23]['score'] +
+        questions[28]['score'] -
+        questions[33]['score'] +
+        questions[38]['score'] -
+        questions[43]['score'] +
+        questions[48]['score']);
+  }
+  openness() {
+    return (8 +
+        questions[4]['score'] -
+        questions[9]['score'] +
+        questions[14]['score'] -
+        questions[19]['score'] +
+        questions[24]['score'] -
+        questions[29]['score'] +
+        questions[34]['score'] -
+        questions[39]['score'] +
+        questions[44]['score'] -
+        questions[49]['score']);
+  }
+  //Initializing build//
 
   @override
   Widget build(BuildContext context) {
@@ -364,10 +423,30 @@ class MyBiGFiveQuizAppState extends State<MyBiGFiveQuizApp> {
                 ),
               ],
             )
-          : Center(
-              child: Text(
-                  'Your Extroversion Score is: ' + extroversion().toString()),
-            ),
+          : Column(
+            children: <Widget>[
+              Center(
+                  child: Text(
+                      'Your Extroversion Score is: ' + extroversion().toString()),
+                ),
+              Center(
+                  child: Text(
+                      'Your Agreeableness Score is: ' + agreeableness().toString()),
+                ),
+              Center(
+                  child: Text(
+                      'Your Conscientiousness Score is: ' + conscientiousness().toString()),
+                ),
+              Center(
+                  child: Text(
+                      'Your Neuroticism Score is: ' + neuroticism().toString()),
+                ),
+              Center(
+                  child: Text(
+                      'Your Openness to Experience Score is: ' + openness().toString()),
+                ),
+            ],
+          ),
     ));
   }
 }
